d982414da6a87312f53e7f59ecc2cb61897b153f
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2022-04-04 19:25:43 +0200

    Update ESP_Arduino_connection.ino

--- Task2/ESP_Arduino_connection/ESP_Arduino_connection.ino
+++ Task2/ESP_Arduino_connection/ESP_Arduino_connection.ino
@@ -4,16 +4,17 @@
 #include <ArduinoJson.h>
 #include <WiFiManager.h>
 
-String username = "Hassan";
-String password = "hassan87890";
+String username = "POCO F3";
+String password = "Hassan112233@";
 String json = " ";
+String response = "asl ha2olk";
 
 void setup() {
- Serial.begin(9600); /* begin serial for debug */
- Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
- 
- WiFi.begin(username, password);
- while (WiFi.status() != WL_CONNECTED) {
+  Serial.begin(9600); /* begin serial for debug */
+  Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
+
+  WiFi.begin(username, password);
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -21,27 +22,32 @@ void setup() {
 
   json = toJSON();
 }
-
 void loop() {
-  postValues(json);
- Wire.beginTransmission(8); /* begin with device address 8 */
- Wire.write("Begin Transmission");  /* sends hello string */
- Wire.endTransmission();    /* stop transmitting */
+  //  postValues(json);
+  response = getValues();
+  Serial.print(response);
+  if (response == "1") {
+    Serial.print("feha l a5feha");
 
- Wire.requestFrom(8, 20); /* request & read data of size 20 from slave */
- while(Wire.available()){
-    char c = Wire.read();
-  Serial.print(c);
- }
- Serial.println();
- delay(1000);
+  } else {
+    Wire.beginTransmission(8); /* begin with device address 8 */
+    Wire.write(response.c_str());  /* sends hello string */
+    Wire.endTransmission();    /* stop transmitting */
+
+    Wire.requestFrom(8, 50000); /* request & read data of size 20 from slave */
+    while (Wire.available()) {
+      char c = Wire.read();
+      Serial.print(c);
+    }
+    Serial.println();
+    delay(1000);
+  }
 }
 
 int postValues(String json) {
   WiFiClient ourClient;
   HTTPClient http; //Declare object of class HTTPClient
-  http.begin(ourClient, "https://flask-testing-iot.herokuapp.com/hassan
-  "); //Specify request destination
+  http.begin(ourClient, "http://192.168.164.126:5000/alo"); //Specify request destination
   http.addHeader("Content-Type", "application/json"); //Specify content-type header
   int httpCode = http.POST(json);//Send the request
   Serial.println(json);
@@ -51,6 +57,18 @@ int postValues(String json) {
   return httpCode;
 }
 
-String toJSON() { 
+String getValues() {
+  WiFiClient ourClient;
+  HTTPClient http; //Declare object of class HTTPClient
+  http.begin(ourClient, "http://192.168.164.126:5000/alo"); //Specify request destination
+  http.addHeader("Content-Type", "application/json"); //Specify content-type header
+  //Serial.print(json);
+  int httpCode = http.GET(); //Send the request
+  String payload = http.getString(); //Get the response payload
+  Serial.println(payload);
+  return payload;
+}
+
+String toJSON() {
   return String("{\"input\": ") + "[" + "1" + "]}";
 }
