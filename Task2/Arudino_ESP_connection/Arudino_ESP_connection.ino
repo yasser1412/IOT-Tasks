3eebda550f32fcfd91f1fbbc87daf46af7c3f0b9
Hassan Fathi Sholqamy <devhassanfathi@gmail.com>
2022-04-05 13:09:29 +0200

    updated esp-arduino code

--- Task2/Arudino_ESP_connection/Arudino_ESP_connection.ino
+++ Task2/Arudino_ESP_connection/Arudino_ESP_connection.ino
@@ -1,37 +1,54 @@
-#include <Wire.h>
+//#include <Wire.h>
+//
+//char response[50];
+//void setup() {
+// Wire.begin(8);                /* join i2c bus with address 8 */
+// Wire.onReceive(receiveEvent); /* register receive event */
+// Wire.onRequest(requestEvent); /* register request event */
+// Serial.begin(9600);           /* start serial for debug */
+//
+// pinMode(LED_BUILTIN, OUTPUT);
+//}
+//
+//void loop() {
+// delay(100);
+//
+// 
+//}
+//
+//// function that executes whenever data is received from master
+//void receiveEvent(int howMany) {
+// while (0 <Wire.available()) {
+//    for(int i = 0; i < 50; i++){
+//      response[i] = Wire.read();
+//    }
+//    Serial.print(response);
+////    char c = Wire.read();      /* receive byte as a character */
+////    Serial.print(c);           /* print the character */
+//  }
+// Serial.println();             /* to newline */
+//// digitalWrite(LED_BUILTIN, HIGH);
+//// delay(5000);
+//// digitalWrite(LED_BUILTIN, LOW);
+//// delay(5000);
+//}
+//
+//// function that executes whenever data is requested from master
+//void requestEvent() {
+// Wire.write("7abibi wala yhemmak");  /*send string on request */
+// 
+//}
 
+//==================================================================
 
+int data;
 
-void setup() {
- Wire.begin(8);                /* join i2c bus with address 8 */
- Wire.onReceive(receiveEvent); /* register receive event */
- Wire.onRequest(requestEvent); /* register request event */
- Serial.begin(9600);           /* start serial for debug */
-
- pinMode(LED_BUILTIN, OUTPUT);
+void setup(){
+  Serial.begin(9600);
 }
 
-void loop() {
- delay(100);
-
- 
-}
-
-// function that executes whenever data is received from master
-void receiveEvent(int howMany) {
- while (0 <Wire.available()) {
-    char c = Wire.read();      /* receive byte as a character */
-    Serial.print(c);           /* print the character */
-  }
- Serial.println();             /* to newline */
- digitalWrite(LED_BUILTIN, HIGH);
-// delay(5000);
-// digitalWrite(LED_BUILTIN, LOW);
-// delay(5000);
-}
-
-// function that executes whenever data is requested from master
-void requestEvent() {
- Wire.write("7abibi wala yhemmak");  /*send string on request */
- 
+void loop(){
+  data = Serial.read();
+  delay(500);
+  Serial.println(data);
 }
--- Task2/ESP_Arduino_connection/ESP_Arduino_connection.ino
+++ Task2/ESP_Arduino_connection/ESP_Arduino_connection.ino
@@ -1,74 +1,95 @@
-#include <Wire.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
-#include <WiFiManager.h>
+//#include <Wire.h>
+//#include <ESP8266WiFi.h>
+//#include <ESP8266HTTPClient.h>
+//#include <ArduinoJson.h>
+//#include <WiFiManager.h>
+//
+//String username = "POCO F3";
+//String password = "Hassan112233@";
+//String json = " ";
+////String response = "12345678901234567890123456789";
+//char response[] = "12345678901234567890123456789012345678901234567890";
+//
+//
+//void setup() {
+// Serial.begin(9600); /* begin serial for debug */
+// Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
+//
+//// for(int i = 0; i < 50; i++){ response[i] = i; }
+// 
+//// WiFi.begin(username, password);
+//// while (WiFi.status() != WL_CONNECTED) {
+////    delay(500);
+////    Serial.print(".");
+////  }
+////  Serial.println("Connected!");
+////
+////  json = toJSON();
+//
+//}
+//void loop() {
+//  //  postValues(json);
+////  response = getValues();
+// 
+//    
+//    Wire.beginTransmission(8); /* begin with device address 8 */
+//    for(int i = 0; i < sizeof(response); i++){
+//      
+//      Wire.write(response[i]);  /* sends string */  
+//      
+//    }
+//    Wire.endTransmission();    /* stop transmitting */
+//    
+//    Wire.requestFrom(8, 20); /* request & read data of size 20 from slave */
+//    while (Wire.available()) {
+//      char c = Wire.read();
+//      Serial.print(c);
+//    }
+//    Serial.println();
+//    delay(1000);
+//}
+//
+//int postValues(String json) {
+//  WiFiClient ourClient;
+//  HTTPClient http; //Declare object of class HTTPClient
+//  http.begin(ourClient, "https://flask-testing-iot.herokuapp.com/hassan"); //Specify request destination
+//  http.addHeader("Content-Type", "application/json"); //Specify content-type header
+//  int httpCode = http.POST(json);//Send the request
+//  Serial.println(json);
+//  String payload = http.getString(); //Get the response payload
+//  Serial.println(payload);
+//  Serial.println(httpCode);
+//  return httpCode;
+//}
+//
+//String getValues() {
+//  WiFiClient ourClient;
+//  HTTPClient http; //Declare object of class HTTPClient
+//  http.begin(ourClient, "http://192.168.164.126:5000/alo"); //Specify request destination
+//  http.addHeader("Content-Type", "application/json"); //Specify content-type header
+//  //Serial.print(json);
+//  int httpCode = http.GET(); //Send the request
+//  String payload = http.getString(); //Get the response payload
+//  Serial.println(payload);
+//  return payload;
+//}
+//
+//String toJSON() {
+//  return String("{\"input\": ") + "[" + "1" + "]}";
+//}
 
-String username = "POCO F3";
-String password = "Hassan112233@";
-String json = " ";
-String response = "asl ha2olk";
+//================================================================
 
-void setup() {
-  Serial.begin(9600); /* begin serial for debug */
-  Wire.begin(D1, D2); /* join i2c bus with SDA=D1 and SCL=D2 of NodeMCU */
+#include <SoftwareSerial.h>
 
-  WiFi.begin(username, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connected!");
+SoftwareSerial s(3,1);
 
-  json = toJSON();
+void setup(){
+  s.begin(9600);
+  Serial.begin(9600);
 }
-void loop() {
-  //  postValues(json);
-  response = getValues();
-  Serial.print(response);
-  if (response == "1") {
-    Serial.print("feha l a5feha");
 
-  } else {
-    Wire.beginTransmission(8); /* begin with device address 8 */
-    Wire.write(response.c_str());  /* sends hello string */
-    Wire.endTransmission();    /* stop transmitting */
-
-    Wire.requestFrom(8, 50000); /* request & read data of size 20 from slave */
-    while (Wire.available()) {
-      char c = Wire.read();
-      Serial.print(c);
-    }
-    Serial.println();
-    delay(1000);
-  }
-}
-
-int postValues(String json) {
-  WiFiClient ourClient;
-  HTTPClient http; //Declare object of class HTTPClient
-  http.begin(ourClient, "http://192.168.164.126:5000/alo"); //Specify request destination
-  http.addHeader("Content-Type", "application/json"); //Specify content-type header
-  int httpCode = http.POST(json);//Send the request
-  Serial.println(json);
-  String payload = http.getString(); //Get the response payload
-  Serial.println(payload);
-  Serial.println(httpCode);
-  return httpCode;
-}
-
-String getValues() {
-  WiFiClient ourClient;
-  HTTPClient http; //Declare object of class HTTPClient
-  http.begin(ourClient, "http://192.168.164.126:5000/alo"); //Specify request destination
-  http.addHeader("Content-Type", "application/json"); //Specify content-type header
-  //Serial.print(json);
-  int httpCode = http.GET(); //Send the request
-  String payload = http.getString(); //Get the response payload
-  Serial.println(payload);
-  return payload;
-}
-
-String toJSON() {
-  return String("{\"input\": ") + "[" + "1" + "]}";
+void loop(){
+  s.write("test");
+  delay(500);
 }
